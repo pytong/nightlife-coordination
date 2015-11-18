b486083e8fcc66543353fcdab8420c57c751d99b
pytong <pytong07@gmail.com>
2015-11-18 02:07:58 +0000

    Do not read .env file if it is on heroku

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-require('dotenv').load(); // Only required for running locally
+// require('dotenv').load(); // Only required for running locally
 
 var express = require('express');
 var routes = require('./app/routes/index.js');
